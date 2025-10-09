6b63aa115a491d3771e0ad4ccce869f65c7c9b5a
skv0zsneg <klimentlamonov@yandex.ru>
2025-10-10 01:12:37 +0300

    Add decorator for db logic functions.

--- mdabojbot/db.py
+++ mdabojbot/db.py
@@ -8,10 +8,27 @@ engine = create_async_engine("sqlite+aiosqlite:///mdabojbot.db", echo=True)
 async_session = async_sessionmaker(engine, expire_on_commit=False)
 
 
+def make_db_session(method):
+    """Decorator for easy ORM queries without session handler."""
+
+    async def wrapper(*args, **kwargs):
+        async with async_session() as session:
+            try:
+                return await method(*args, session=session, **kwargs)
+            except Exception as e:
+                await session.rollback()
+                raise e
+            finally:
+                await session.close()
+
+    return wrapper
+
+
 async def init_db(application: Application):
     # TODO: Maybe change to something beautiful?
     from mdabojbot.prediction.models import Prediction  # noqa
     from mdabojbot.rank.models import Rank, UsersRanks  # noqa
+    from mdabojbot.user.models import User  # noqa
 
     async with engine.begin() as conn:
         await conn.run_sync(BaseModel.metadata.create_all)
